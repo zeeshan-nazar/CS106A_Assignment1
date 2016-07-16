829b94128e3988d7a1c4a263a9339fc375995d0a
zeeshan-nazar <mzeeshan.nazar@gmail.com>
2016-07-16 05:31:12 +0500

    Problem 1 solution

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -13,5 +13,24 @@ import stanford.karel.*;
 public class CollectNewspaperKarel extends SuperKarel {
 
 	// You fill in this part
+	public void run(){
+		
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+		move();
+		pickBeeper();
+		turnRight();
+		turnRight();
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+		move();
+		turnAround();
+	}
 
 }
